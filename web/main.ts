6cd466fb65dd42d1430e383d510d4fa557532688
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-10-31 21:36:44 +0700

    Autoreload setup

--- web/main.ts
+++ web/main.ts
@@ -1,13 +1,35 @@
 import { exec } from "child_process";
 import express from "express"
 import { resolve } from "path"
+import http from "http"
 
+import livereload from "livereload"
+import connectLivereload from "connect-livereload"
 
 const STATIC = "./page"
 const EXE = "./bin/main"
 
 const app = express();
-app.use(express.static(resolve(__dirname, STATIC)));
+const server = http.createServer(app);
+
+if(true){
+    const livereloadPort = 35729;
+    app.use(connectLivereload({ port: livereloadPort }));
+    const livereloadServer = livereload.createServer({ port: livereloadPort }, () => {
+        const fnOut = (socket: WebSocket) => {
+            const fnIn = ({ data }: any) => {
+                if(JSON.parse(data).command === "hello"){
+                    livereloadServer.refresh(".")
+                    socket.removeEventListener("message", fnIn)
+                }
+            }
+           socket.addEventListener("message", fnIn)
+           livereloadServer.server.removeListener("connection", fnOut)
+        }
+        livereloadServer.server.addListener("connection", fnOut)
+    })
+    livereloadServer.watch(resolve(__dirname, STATIC))
+}
 
 app.get("/api", (req, res) => {
     exec(resolve(__dirname, EXE), (e, stdout) => {
@@ -15,6 +37,7 @@ app.get("/api", (req, res) => {
     });
 });
 
-app.listen(80, () => {
+app.use(express.static(resolve(__dirname, STATIC)));
+server.listen(80, () => {
     console.log("Serving at http://localhost:80");
 });
